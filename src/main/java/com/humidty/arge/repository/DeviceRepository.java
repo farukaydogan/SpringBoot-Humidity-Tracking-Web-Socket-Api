d22653fda9d1a1838129475190c91ba4d2dd3399
farukaydogan <faruk034595@gmail.com>
2023-07-20 10:13:47 +0300

    folder directory changed

--- src/main/java/com/humidty/arge/repository/DeviceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.humidty.arge.repository;
-
-import com.humidty.arge.model.Device;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DeviceRepository extends MongoRepository<Device,String> {
-
-}
